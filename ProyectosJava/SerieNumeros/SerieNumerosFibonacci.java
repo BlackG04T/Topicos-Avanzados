cd2675461f617325cf69c979909a4ebe8f4b300a
BlackG04T <zetinaluis03@gmail.com>
2021-09-15 03:12:27 -0500

    changed SerieNumerosFibonacci

--- ProyectosJava/SerieNumeros/SerieNumerosFibonacci.java
+++ ProyectosJava/SerieNumeros/SerieNumerosFibonacci.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class SerieNumerosFibonacci{
 	public static void main(String[] args) {
 		int numero1 = 0;
@@ -5,6 +7,10 @@ class SerieNumerosFibonacci{
 		int suma = 0;
 		int cantidadNumeros = 10;
 
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Ingrese la cantidad de numeros a mostrar: ");
+		cantidadNumeros = reader.nextInt(); 
+		System.out.println("-----");
 		for (int x = 0; x<=cantidadNumeros ; x++ ) {
 			suma = numero1 + numero2;
 			numero1 = numero2;
